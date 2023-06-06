1925c20e0c86987ac655671cd57af22fb653eea8
John Sanabria <john.sanabria@correounivalle.edu.co>
2023-06-06 07:39:36 -0500

    adicionando pausa en la ejecucion

--- requests_example.py
+++ requests_example.py
@@ -8,10 +8,12 @@ ID = "1"
 # Envio peticion 'POST'
 response = requests.post(URL, json={ "content": "escribir libro" })
 print(f"Respuesta a post {response}")
+input("Presione ENTER para continuar...")
 
 # Envio peticion 'PUT'
 response = requests.put(URL + f"/{ID}", json={ "content": "escribir libro sobre la web" })
 print(f"Respuesta a put {response}")
+input("Presione ENTER para continuar...")
 
 # Envio peticion 'DELETE'
 response = requests.delete(URL + f"/{ID}")
